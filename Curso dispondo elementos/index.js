0e6145018e70b7d4944374e5d800dacb2d51044a
William Alves <109997418+WilliamAlves007@users.noreply.github.com>
2022-08-03 19:45:06 -0300

    Delete Curso dispondo elementos directory

--- Curso dispondo elementos/index.js
+++
@@ -1,5 +0,0 @@
-const botaomenu = document.querySelector('.cabecalho__menu');
-const menu = document.querySelector('.menu__lateral');
-
-botaomenu.addEventListener('click', () => { menu.classList.toggle('menu-lateral--ativo')})
-
